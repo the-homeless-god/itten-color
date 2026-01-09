740c8498277a89bd7f869d7b770865e08aeef393
Marat Zimnurov <zimtir@mail.ru>
2026-01-10 00:18:42 +0300

    chore(ts): solve linting errors

--- solutions/ts/src/index.ts
+++ solutions/ts/src/index.ts
@@ -1,5 +1,6 @@
-import { Color } from './types'
 import { MAX_BYTE_COLOR_VALUE } from './constants'
+
+import { Color } from './types'
 
 const decrementColorNumber = (colorNumber: number): number => {
   return MAX_BYTE_COLOR_VALUE - colorNumber
@@ -7,8 +8,8 @@ const decrementColorNumber = (colorNumber: number): number => {
 
 export const getIttenColor = (color: Color) => {
   return {
-    r: decrementColorNumber(color.r),
-    g: decrementColorNumber(color.g),
     b: decrementColorNumber(color.b),
+    g: decrementColorNumber(color.g),
+    r: decrementColorNumber(color.r),
   }
 }
